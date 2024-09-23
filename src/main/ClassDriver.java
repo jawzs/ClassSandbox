6aadd7975bb5713292e15cdbba709d3d09852ef7
Jonathon Cihlar <36781511+jonathoncihlar@users.noreply.github.com>
2024-09-22 20:16:58 -0600

    Create ClassDriver.java

---
+++ src/main/ClassDriver.java
@@ -0,0 +1,16 @@
+/**
+ * This file instantiates and tests your Java class file.
+ * @author
+ * @version 1.0
+ * @since 2024-xx-xx
+ */
+
+
+class ClassDriver {
+  public static void main(String args[]) {
+
+    // TODO: create an instance of your class
+
+    // TODO: test the methods of your new object
+  }
+}
